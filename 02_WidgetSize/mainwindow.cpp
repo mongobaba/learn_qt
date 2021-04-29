7f2182cda72956631afb36750f12dc1df77599ad
mongobaba <chaowei.meng@gmail.com>
2021-04-29 11:54:46 +0800

    增加WidgetSize示例

---
+++ 02_WidgetSize/mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle(tr("Qt窗体应用"));
+    this->setMaximumSize(300, 300);
+    this->setMinimumSize(300, 300);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
