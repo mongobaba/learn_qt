54633d7d153e47ad5c9ebdc1c0056396d613b67b
mongobaba <chaowei.meng@gmail.com>
2021-04-30 08:29:13 +0800

    添加QLineEdit示例

---
+++ 11_QLineEdit/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    lineEdit = new QLineEdit(this);
+    lineEdit->setGeometry(QRect(100, 100, 250, 25));
+    lineEdit->setStyleSheet("border:1px;border-style:solid;color:red;border-color:bluered;");
+    lineEdit->setMaxLength(12);
+    lineEdit->setEchoMode(QLineEdit::Password);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
