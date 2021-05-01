1599b4b333c1d0c72714c1ddf5ddcb14d752d79b
mongobaba <chaowei.meng@gmail.com>
2021-05-01 14:14:18 +0800

    增加ReadIniFile示例

---
+++ 43_ReadIniFile/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QGridLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QMainWindow>
+#include <QPushButton>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void readFile();
+
+private:
+    Ui::MainWindow *ui;
+    QGridLayout* layout;
+    QWidget* widget;
+    QLabel* fileLabel;
+    QLineEdit* filePath;
+    QLabel* nodeLabel;
+    QLineEdit* nodeName;
+    QLabel* keyLabel;
+    QLineEdit* key;
+    QLabel* valueLabel;
+    QLineEdit* value;
+    QPushButton* button;
+};
+#endif // MAINWINDOW_H
