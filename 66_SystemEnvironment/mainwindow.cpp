e695c7f6eb30542e49f7fdcf56802f1e862b4453
mongobaba <chaowei.meng@gmail.com>
2021-05-01 23:20:58 +0800

    增加SystemEnvironment示例

---
+++ 66_SystemEnvironment/mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QListView>
+#include <QProcess>
+#include <QStringListModel>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QStringList list = QProcess::systemEnvironment();
+    QStringListModel* model = new QStringListModel(list);
+    QListView* view = new QListView(this);
+//    view->setGeometry(QRect(10, 10, 380, 300));
+    view->setModel(model);
+    setCentralWidget(view);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
