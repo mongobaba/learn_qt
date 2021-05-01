676e313cd792f6107a0f7d3b2e1ba1a2bd1299c7
mongobaba <chaowei.meng@gmail.com>
2021-05-01 16:30:26 +0800

    增加MovePicture示例

---
+++ 50_MovePicture/mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QPixmap>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    label = new QLabel(this);
+    label->setGeometry(QRect(50, 50, 300, 300));
+
+    image = new QImage();
+    image->load(":/new/prefix1/butterfly.png");
+    label->setPixmap(QPixmap::fromImage(*image));
+
+    button = new QPushButton(tr("移动"), this);
+    button->setGeometry(QRect(50, 400, 80, 25));
+    connect(button, SIGNAL(clicked()), this, SLOT(moveImage()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::moveImage()
+{
+    static int i = 50;
+    i += 50;
+    label->move(i, 50);
+}
---
+++ 50_MovePicture/mainwindow.h
@@ -0,0 +1,29 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QLabel>
+#include <QMainWindow>
+#include <QPushButton>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void moveImage();
+
+private:
+    Ui::MainWindow *ui;
+    QLabel* label;
+    QImage* image;
+    QPushButton* button;
+};
+#endif // MAINWINDOW_H
