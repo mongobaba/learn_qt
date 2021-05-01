bbffc11c3b42eb99f2c0f51c9fe35b5602cccc1b
mongobaba <chaowei.meng@gmail.com>
2021-05-01 12:41:12 +0800

    增加DeleteFile示例

---
+++ 40_DeleteFile/mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFileDialog>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    filePath = new QLineEdit(this);
+    filePath->setGeometry(QRect(50, 50, 200, 25));
+
+    browseButton = new QPushButton(tr("浏览文件"), this);
+    browseButton->setGeometry(QRect(270, 50, 80, 25));
+    connect(browseButton, SIGNAL(clicked()), this, SLOT(browseFile()));
+
+    deleteButton = new QPushButton(tr("删除文件"), this);
+    deleteButton->setGeometry(QRect(50, 100, 80, 25));
+    connect(deleteButton, SIGNAL(clicked()), this, SLOT(deleteFile()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::browseFile()
+{
+    const char* filter = "text file(*.txt);;C file(*.cpp);;All file(*.*)";
+    QString str(QFileDialog::getOpenFileName(this, tr("open file"), "/", filter));
+    filePath->setText(str);
+}
+
+void MainWindow::deleteFile()
+{
+    if (QFile::remove(filePath->text()))
+    {
+        QMessageBox::warning(this, tr("删除文件"), tr("删除文件成功！"));
+    }
+}
