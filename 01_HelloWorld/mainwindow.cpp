809f78e16ec11e6754cc5453e34743920b125176
mongobaba <chaowei.meng@gmail.com>
2021-05-02 23:51:56 +0800

    增加对setGeometry的说明

--- 01_HelloWorld/mainwindow.cpp
+++ 01_HelloWorld/mainwindow.cpp
@@ -10,6 +10,9 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     QLabel* label = new QLabel(this);
     label->setText("Hello, world");
+    // 方式一，其实也是调用下面的形式
+    // label->setGeometry(50, 50, 200, 25);
+    // 方式二
     label->setGeometry(QRect(50, 50, 200, 25));
 }
 
