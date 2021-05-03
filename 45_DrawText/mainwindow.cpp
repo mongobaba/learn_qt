8ffeadeab79da6834b7f69550c1dab12aaa21a91
mongobaba <chaowei.meng@gmail.com>
2021-05-03 17:35:56 +0800

    添加背景色设置

--- 45_DrawText/mainwindow.cpp
+++ 45_DrawText/mainwindow.cpp
@@ -11,9 +11,11 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     QGraphicsScene* scene = new QGraphicsScene();
+    // 切换下面两行代码查看背景色和前景色的差异
+    // scene->setBackgroundBrush(QColor(0, 255, 255, 127));
     scene->setForegroundBrush(QColor(0, 255, 255, 127));
-    QFont font("黑体", 60);
-    scene->addSimpleText("图形图像", font);
+    QFont font(tr("黑体"), 60);
+    scene->addSimpleText(tr("图形图像"), font);
 
     QGraphicsView* view = new QGraphicsView(scene);
     this->setCentralWidget(view);
