3113aa0a6e64c050476e236349e5bd03b182e79e
mongobaba <chaowei.meng@gmail.com>
2021-05-03 12:20:27 +0800

    增加一些说明信息

--- 30_SysTray/mainwindow.cpp
+++ 30_SysTray/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDesktopWidget>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,6 +15,7 @@ MainWindow::MainWindow(QWidget *parent)
         trayIcon = new QSystemTrayIcon(this);
         trayIcon->setIcon(QIcon(":/new/prefix1/pygame.ico"));
         trayIcon->setToolTip(tr("Qt托盘图标功能"));
+        // windows xp那种气泡效果，但是在win10系统上，这一行好像没什么用
         trayIcon->showMessage(tr("托盘"), tr("托盘管理"), QSystemTrayIcon::Information, 10000);
         trayIcon->setContextMenu(menu);
         trayIcon->show();
@@ -32,7 +35,9 @@ void MainWindow::createMenu()
     quitAct = new QAction(tr("退出(&Q)"), this);
     connect(quitAct, SIGNAL(triggered()), qApp, SLOT(quit()));
 
-    menu = new QMenu((QWidget*)QApplication::desktop());
+    // 如果不包含QDesktopWidget头文件，这里需要增加一个到QWidget*
+    // 的强制类型转换，因为编译器不知道这两个类之间的继承关系
+    menu = new QMenu(QApplication::desktop());
     menu->addAction(restoreAct);
     menu->addSeparator();
     menu->addAction(quitAct);
