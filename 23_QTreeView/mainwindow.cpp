d33c0cae9771b7d0a242265df13b48cd0128b65d
mongobaba <chaowei.meng@gmail.com>
2021-05-03 10:08:14 +0800

    为model设置parent，增加对item内存管理的说明

--- 23_QTreeView/mainwindow.cpp
+++ 23_QTreeView/mainwindow.cpp
@@ -10,12 +10,13 @@ MainWindow::MainWindow(QWidget *parent)
     treeView = new QTreeView(this);
     treeView->setGeometry(QRect(50, 50, 350, 200));
 
-    model = new QStandardItemModel(3, 2);
+    model = new QStandardItemModel(3, 2, treeView);
     model->setHeaderData(0, Qt::Horizontal, tr("第一列"));
     model->setHeaderData(1, Qt::Horizontal, tr("第二列"));
 
     QStandardItem* item1 = new QStandardItem(tr("数学"));
     item1->setIcon(QIcon(":/new/prefix1/folder.ico"));
+    // model会管理所有的item
     model->setItem(0, 0, item1);
 
     QStandardItem* item2 = new QStandardItem(tr("语文"));
