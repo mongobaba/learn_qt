7a1bab60ce70cdc43dd04b1c0a15a972e8214193
mongobaba <chaowei.meng@gmail.com>
2021-05-03 15:07:28 +0800

    更改开始按钮的行为

--- 35_QProgressBar/mainwindow.cpp
+++ 35_QProgressBar/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+constexpr int MaxValue = 100000;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -8,8 +10,8 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     bar = new QProgressBar(this);
-    bar->setGeometry(QRect(50, 50, 200, 20));
-    bar->setRange(0, 10000-1);
+    bar->setGeometry(QRect(50, 50, 200, 23));
+    bar->setRange(0, MaxValue - 1);
     bar->setValue(0);
 
     button = new QPushButton(tr("开始"), this);
@@ -24,12 +26,10 @@ MainWindow::~MainWindow()
 
 void MainWindow::startBar()
 {
-    for (int i = 0; i < 10000; i++)
+    button->setEnabled(false);
+    for (int i = 0; i < MaxValue; i++)
     {
-        if (i == 9999)
-        {
-            button->setText(tr("结束"));
-        }
         bar->setValue(i);
     }
+    button->setEnabled(true);
 }
