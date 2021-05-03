a63efdd13f11573185443d8e31eb65a67c46b33e
mongobaba <chaowei.meng@gmail.com>
2021-05-03 22:11:32 +0800

    增加旧式做法的说明

--- 62_ScreenSize/mainwindow.cpp
+++ 62_ScreenSize/mainwindow.cpp
@@ -3,6 +3,7 @@
 
 #include <QLabel>
 #include <QScreen>
+#include <QDesktopWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,10 +14,13 @@ MainWindow::MainWindow(QWidget *parent)
     QLabel* label = new QLabel(this);
     label->setGeometry(QRect(50, 50, 250, 25));
 
+    // 下面这种方式已经被废弃了
+    // QApplication::desktop()->screenGeometry();
+
     QList<QScreen*> screens = QGuiApplication::screens();
     QRect rect = screens.at(0)->geometry();
-    QString str = tr("主屏幕分辨率为：%1*%2");
-    label->setText(str.arg(rect.width()).arg(rect.height()));
+    QString format = tr("主屏幕分辨率为：%1*%2");
+    label->setText(format.arg(rect.width()).arg(rect.height()));
 }
 
 MainWindow::~MainWindow()
